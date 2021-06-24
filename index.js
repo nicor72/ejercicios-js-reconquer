eba6230e80cae331432a3b16bc1b29ca35a80f67
nico <nroman@eclass.cl>
2021-06-24 11:22:37 -0400

    ejercicio 2

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-console.log('vamoooo equipo')
+console.log('----------------------------------------------')
+console.log('Ejercicio 2')
+
+// Declarar un arreglo que contenga los siguientes verbos: ['jugar', 'anotar', 'pegar']
+// TIP: deben ser strings y pueden ser en cualquier tiempo verbal (ej: jugó)
+
+
+// Declarar un objeto que contenga lo siguiente:
+// Nombre de tu jugador favorito de esta copa america
+// Número de partidos jugados en la copa
+// Número de goles anotados en la copa
+// Número de faltas en la copa
+
+
+// Acceder directamente al nombre del jugador, al verbo jugar y al número de partidos jugados y mostrar en consola
+// TIP: Usar un solo console.log y se debe formar una oración del tipo "jugador jugó 3"
+
+
+// Destructurar el nombre del jugador, el verbo pegar y el número de faltas en la copa y mostrar en consola
+// TIP: Se debe destructurar antes del console.log y se debe formar una oración del tipo "jugador pega 10"
+
+
+// BONUS
+// crear una función y ejecutarla, que reciba el nombre del jugador, un verbo y una propiedad del jugador
+// para mostrar en consola una oración del tipo: 'jugador anota 1'
+// BONUS BONUS
+// agregar alfinal de la oración un string que complete la oración ej: jugador anota 1 'gol'
+
+
+// BONUS BONUS BONUS (CRACK)
+// Recibir en la función con rest parameters
+
+
+
+console.log('----------------------------------------------')
+console.log('Ejercicio 1')
+
 // Inicializar una vareable para contar el número de lesiones
 // Elegir entre var o let. Justifique su respuesta. 😬
 
+
 // Declarar una función que sume nuevas lesiones y muestre en consola el valor del contador de lesiones
 // Tip: la funcion debe recibir un parametro.
+
+
 // Ejecutar la función para que el contador muestre 64 lesiones
+
 
 // BONUS
 // Ejecutar dos veces más la funcion hasta llegar a 100 lesiones
+
