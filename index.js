1d8fc1d8034f63fe3477cda3e73ed91b675204a0
camilo <camilo.alarcon@me.com>
2021-07-07 15:16:29 -0400

    respuestas ejercicio 3

--- index.js
+++ index.js
@@ -1,49 +1,138 @@
-// Declarar un arreglo que contenga los siguientes verbos: ['jugar', 'anotar', 'pegar']
-// TIP: deben ser strings y pueden ser en cualquier tiempo verbal (ej: jugó)
+console.log('----------------------------------------------')
+console.log('Ejercicio 3')
 
-const verbos = ['jugó', 'anotó', 'pegó']
+console.log('Paso 1: jugadores')
+// Recorrer el array jugadores con un bucle for desde el principio
+// Saltandose el segundo jugador y terminando el ciclo en el quinto jugador
+// En cada ciclo mostrar el nombre del jugador en consola
 
-// Declarar un objeto que contenga lo siguiente:
-// Nombre de tu jugador favorito de esta copa america
-// Número de partidos jugados en la copa
-// Número de goles anotados en la copa
-// Número de faltas en la copa
+const jugadores = ['Salas','Zamorano', 'Sanchez', 'Vidal','Medel','Bravo']
 
-const jugador = { nombre: 'vargas', partidos: 6, goles: 3, faltas: 2}
-
-// Acceder directamente al nombre del jugador, al verbo jugar y al número de partidos jugados y mostrar en consola
-// TIP: Usar un solo console.log y se debe formar una oración del tipo "jugador jugó 3"
-
-console.log(jugador.nombre, verbos[0], jugador.partidos)
-
-// Destructurar el nombre del jugador, el verbo pegar y el número de faltas en la copa y mostrar en consola
-// TIP: Se debe destructurar antes del console.log y se debe formar una oración del tipo "jugador pega 10"
-
-const { nombre, faltas } = jugador
-const [,,pegó] = verbos
-console.log(nombre, pegó, faltas)
-
-// BONUS
-// crear una función y ejecutarla, que reciba el nombre del jugador, un verbo y una propiedad del jugador
-// para mostrar en consola una oración del tipo: 'jugador anota 1'
-// BONUS BONUS
-// agregar alfinal de la oración un string que complete la oración ej: jugador anota 1 'gol'
-
-function oracion (nombre, verbo, propiedad ){
-    console.log (nombre, verbo, propiedad , 'goles')
-    }
-    oracion(jugador.nombre, verbos[1], jugador.goles)
-
-// BONUS BONUS BONUS (CRACK)
-// Recibir en la función con rest parameters
-
-function oracion2(...params){
-    console.log(params[0], params[1],params[2], 'goles')
+for (let i=0; i<jugadores.length; i++){
+    if (i=== 1)
+    continue;  
+    console.log(jugadores[i])
 }
-oracion2(jugador.nombre, verbos[1], jugador.goles)
+
+console.log('')
+console.log('Paso 2: mediciones')
+// Recorrer el array mediciones con un bucle for desde el principio hasta el final
+// En cada ciclo mostrar el tipo de cada medicion junto al valor de la medición en consola
+// TIP: usar typeof()
+const mediciones = [100, '86', 1.4, '5.5', 66, true, 'Positivo']
+
+for (let i=0; i<mediciones.length; i++){
+    console.log(typeof(mediciones[i]), mediciones[1])
+}
+
+console.log('')
+console.log('Paso 3: conversión')
+// Recorrer nuevamente el array de mediciones 👆 de principio a fin
+// En cada ciclo transformar a tipo String los tipos Int y mostrar en consola
+// TIP: usar .toString()
+// BONUS: En en mismo bucle for pasar los tipo String a tipo Int con parseInt()
+// BONUS BONUS: En en mismo bucle for pasar los tipo String a tipo Int con parseInt()
+
+for (let i=0; i<mediciones.length; i++){
+}
+
+console.log('')
+console.log('Paso 4: Doble loop')
+// BONUS BONUS BONUS (CRACK)
+// Usando los arrays ya declarados, recorrer el array jugadores y por cada jugador recorrer todas las mediciones
+// TIP: usar un bucle for dentro otro bucle for
 
 
-//////////////////////////////////////
+for (let i=0; i<jugadores.length; i++){
+    console.log(jugadores[i])
+    for (let i=0; i<mediciones.length; i++){
+        console.log(mediciones[i])
+    
+    }
+}
+
+
+
+/////////////////////////////////////
+
+
+// for (let i=0; i<5; i++){
+//     if (i=== 1)
+//     continue;   // el break termina todo  // el continue , se lo salta y sigue
+//     console.log(i);
+// }
+
+// for (let i=5; i<=10; i++){
+//     console.log(i);
+// }
+
+
+// for (let i=0; i<=10; i=i+2){
+//     if(i===2)
+//     continue;
+//     console.log(i);
+//     if(i===6)
+//     break;    
+// }
+
+
+// const verbos = ['jugó', 'anotó', 'pegó']
+
+// for (let i=0; i<verbos.length; i++){
+//     console.log(verbos[i])
+// }
+
+
+
+// ////////////////////////////////////
+
+// // Declarar un arreglo que contenga los siguientes verbos: ['jugar', 'anotar', 'pegar']
+// // TIP: deben ser strings y pueden ser en cualquier tiempo verbal (ej: jugó)
+
+// // const verbos = ['jugó', 'anotó', 'pegó']
+
+
+// // Declarar un objeto que contenga lo siguiente:
+// // Nombre de tu jugador favorito de esta copa america
+// // Número de partidos jugados en la copa
+// // Número de goles anotados en la copa
+// // Número de faltas en la copa
+
+// const jugador = { nombre: 'vargas', partidos: 6, goles: 3, faltas: 2}
+
+// // Acceder directamente al nombre del jugador, al verbo jugar y al número de partidos jugados y mostrar en consola
+// // TIP: Usar un solo console.log y se debe formar una oración del tipo "jugador jugó 3"
+
+// console.log(jugador.nombre, verbos[0], jugador.partidos)
+
+// // Destructurar el nombre del jugador, el verbo pegar y el número de faltas en la copa y mostrar en consola
+// // TIP: Se debe destructurar antes del console.log y se debe formar una oración del tipo "jugador pega 10"
+
+// const { nombre, faltas } = jugador
+// const [,,pegó] = verbos
+// console.log(nombre, pegó, faltas)
+
+// // BONUS
+// // crear una función y ejecutarla, que reciba el nombre del jugador, un verbo y una propiedad del jugador
+// // para mostrar en consola una oración del tipo: 'jugador anota 1'
+// // BONUS BONUS
+// // agregar alfinal de la oración un string que complete la oración ej: jugador anota 1 'gol'
+
+// function oracion (nombre, verbo, propiedad ){
+//     console.log (nombre, verbo, propiedad , 'goles')
+// }
+//     oracion(jugador.nombre, verbos[1], jugador.goles)
+
+// // BONUS BONUS BONUS (CRACK)
+// // Recibir en la función con rest parameters
+
+// function oracion2(...params){
+//     console.log(params[0], params[1],params[2], 'goles')
+// }
+// oracion2(jugador.nombre, verbos[1], jugador.goles)
+
+
+// //////////////////////////////////////
 
 
 
